c8a1518b3e477572a93d4700185c3b77abc7c336
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-12-04 11:52:15 +0000

    add sidebar to CV display containing skills profile. Need to add skills input to editor

--- src/App.jsx
+++ src/App.jsx
@@ -100,50 +100,72 @@ function App() {
       {/* CV DISPLAY PAGE */}
       <div className={`cv-page-ctnr ${cvPageVisibility}`}>
         <div className="cv-page">
-          <header>
+          <header className="cv-header">
             <h1 className="cv-name">{data.name}</h1>
             <div className="contact-details">
               <p className="cv-email">{data.email}</p>
               <p className="cv-number">{data.number}</p>
             </div>
           </header>
-          {data.statement !== "" && (
-            <section className="cv-personal-statement">
-              <h2>Personal Statement</h2>
-              <p>{data.statement}</p>
-            </section>
-          )}
-          <section className="cv-experience">
-            <h2>Work Experience</h2>
-            {jobData.map((job, index) => {
-              return (
-                <div key={index}>
-                  <h3>{job.company + " - " + job.title}</h3>
-                  <p className="cv-dates">
-                    {job["job-start"] + " to " + job["job-end"]}
-                  </p>
-                  <h4>Responsibilities:</h4>
-                  <ul>
-                    {job["first-resp"] !== "" && <li>{job["first-resp"]}</li>}
-                    {job["second-resp"] !== "" && <li>{job["second-resp"]}</li>}
-                    {job["third-resp"] !== "" && <li>{job["third-resp"]}</li>}
-                    {job["fourth-resp"] !== "" && <li>{job["fourth-resp"]}</li>}
-                    {job["fifth-resp"] !== "" && <li>{job["fifth-resp"]}</li>}
-                  </ul>
-                </div>
-              );
-            })}
-          </section>
-          <section className="cv-education">
-            <h2>Education</h2>
-            <h3>
-              {data.school +
-                `(${data.award}): ` +
-                data["school-start"] +
-                " to " +
-                data["school-end"]}{" "}
-            </h3>
-          </section>
+          <main>
+            <div className="main-content">
+              {data.statement !== "" && (
+                <section className="cv-personal-statement">
+                  <h2>Personal Statement</h2>
+                  <p>{data.statement}</p>
+                </section>
+              )}
+              <section className="cv-experience">
+                <h2>Work Experience</h2>
+                {jobData.map((job, index) => {
+                  return (
+                    <div key={index}>
+                      <h3>{job.company + " - " + job.title}</h3>
+                      <p className="cv-dates">
+                        {job["job-start"] + " to " + job["job-end"]}
+                      </p>
+                      <h4>Responsibilities:</h4>
+                      <ul>
+                        {job["first-resp"] !== "" && (
+                          <li>{job["first-resp"]}</li>
+                        )}
+                        {job["second-resp"] !== "" && (
+                          <li>{job["second-resp"]}</li>
+                        )}
+                        {job["third-resp"] !== "" && (
+                          <li>{job["third-resp"]}</li>
+                        )}
+                        {job["fourth-resp"] !== "" && (
+                          <li>{job["fourth-resp"]}</li>
+                        )}
+                        {job["fifth-resp"] !== "" && (
+                          <li>{job["fifth-resp"]}</li>
+                        )}
+                      </ul>
+                    </div>
+                  );
+                })}
+              </section>
+              <section className="cv-education">
+                <h2>Education</h2>
+                <h3>{data.award + " at " + data.school}</h3>
+                <p className="cv-dates">
+                  {data["school-start"] + " to " + data["school-end"]}
+                </p>
+              </section>
+            </div>
+            <aside>
+              <h2>Skills Profile</h2>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVASCRIPT WITT A TON O SHIT </li>
+                <li>REACT</li>
+                <li>NPM</li>
+                <li>GIT</li>
+              </ul>
+            </aside>
+          </main>
         </div>
         <button type="button" className="edit-cv-button" onClick={handleEdit}>
           Edit CV
